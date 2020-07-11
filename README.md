68103b4ae487edc9e3b808225bc70a602938bf84
Daric Wichers <daric.wichers@gmail.com>
2020-07-10 22:33:29 -0700

    simple pixel replacement.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# usf-mosquitos
+# Install Git
+
+(Git Download)[https://git-scm.com/downloads]
+
+# Install Anaconda
+
+(Anaconda Download)[https://www.anaconda.com/products/individual]
+
+# Install usf-mosquitos
+
+1. Clone usf-mosquito code
+```bash
+git clone git@github.com:oneluckypic/usf-mosquitos.git
+```
+
+2. Activate conda base environment
+```bash
+conda activate base
+```
+
+# Run Script to Filter Pixel Band
+
+Example:
+```bash
+python replace_band.py -b '[182,200,183,255]' -f SuisunMarsh2020_Orthomosaic_export_WedMay06033950.811478.tif
+```
+
+Print Help:
+```bash
+python replace_band.py --help
+```
---
+++ replace_band.py
@@ -0,0 +1,47 @@
+''' This script is a simple replacement of pixel values.  Given a pixel band and
+    range this script will find pixels that fall with in the band's range and replace
+    them with another pixel 4-channel set of values (by default black).
+    
+    python replace_band.py --help
+'''
+import click
+import json
+import numpy as np
+import os
+from PIL import Image
+
+compressions = ["raw", "tiff_ccitt", "tiff_lzw", "jpeg",
+                "tiff_adobe_deflate", "tiff_raw_16",  "packbits", "tiff_thunderscan",
+                "tiff_deflate", "tiff_sgilog", "tiff_sgilog24"]
+
+@click.command()
+@click.option('-f', '--file', type=click.Path(), required=True, help='Tiff image file.')
+@click.option('-b', '--band', required=True,
+              help='List of 4-channel pixels to replace, e.g. \'[182, 200, 183, 255]\'.')
+@click.option('-t', '--to-band', default='[0, 0, 0, 0]',
+              help='List of 4-channel pixels to replace with.')
+@click.option('-r', '--rng', default='[30, 30, 30, 30]',
+              help='band plus or minus range will be replaced.')
+@click.option('-p', '--prefix', default='mod_', help='prefix to place on modified Tiff file.')
+@click.option('-c', '--compression', default='tiff_lzw',
+              help='Compression to use to save Tiff file. Options: ' + ', '.join(compressions))
+def replace_band(file, band, to_band, rng, prefix, compression):
+    band = json.loads(band)
+    to_band = json.loads(to_band)
+    rng = json.loads(rng)
+    
+    Image.MAX_IMAGE_PIXELS = None
+    im = Image.open(file)
+    imarray = np.array(im)
+    w = np.where((band[0] - rng[0] < imarray[:, :, 0]) & (imarray[:, :, 0] < band[0] + rng[0]) &
+                 (band[1] - rng[1] < imarray[:, :, 1]) & (imarray[:, :, 1] < band[1] + rng[1]) &
+                 (band[2] - rng[2] < imarray[:, :, 2]) & (imarray[:, :, 2] < band[2] + rng[2]) &
+                 (band[3] - rng[3] < imarray[:, :, 3]) & (imarray[:, :, 3] < band[3] + rng[3]))
+    imarray[w] = to_band
+    im = Image.fromarray(imarray)
+    to_file = os.path.join(os.path.dirname(file), prefix + os.path.basename(file))
+    im.save(to_file, compression=compression)
+
+
+if __name__ == '__main__':
+    replace_band()
